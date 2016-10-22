1f1cd8ff8c31b11a6d045d5ca20bc0abe577318e
Alla Klimova <alla_88@inbox.lv>
2016-10-22 15:49:02 -0300

    Pokemon

--- test2.js
+++ test2.js
@@ -1,5 +1,24 @@
-function sqr( ...params ) { 
-return params.map(i => i * i);
+class Pocemon {
+ constructor(pocemon) {
+  this.name = pocemon.name;
+  this.level = pocemon.level;
  }
-var result = sqr(2, 3, 4);
- console.log(result); 
+}
+
+class PocemonList {
+ constructor(params = []) {
+  console.log('params:', params);
+  this.list = params;
+ }
+}
+
+const pocemonsA = [
+ {name: 'Molly', level: 1},
+ {name: 'Mike', level: 0},
+ {name: 'Anny', level: 2}
+];
+
+const lostList = pocemonsA.map(value => new Pocemon(value));
+
+const lost = new PocemonList(lostList);
+const found = new PocemonList();
